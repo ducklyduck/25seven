25ff41b4d29e9df31ad56ad5212715792733fdbe
ducklyduck <nivanov475@gmail.com>
2024-06-08 14:02:47 +0500

    added dropdown select; working fine

--- src/screens/ChangeTask.js
+++ src/screens/ChangeTask.js
@@ -5,6 +5,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import DatePicker from 'react-native-date-picker';
 import {Icon, Button} from '@rneui/themed';
 import {useTaskListStore} from '../utils/store';
+import SelectDropdown from 'react-native-select-dropdown';
 
 const styles = StyleSheet.create({
   container: {
@@ -28,6 +29,27 @@ const styles = StyleSheet.create({
     gap: 15,
     marginBottom: 15,
   },
+  dateButton: {
+    borderColor: 'coral',
+    borderWidth: 1
+  },
+  projectButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 8,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    borderColor: 'indigo',
+    borderWidth: 1,
+  },
+  projectText: {
+    color: 'indigo',
+    fontSize: 16,
+    textAlign: 'center',
+    paddingVertical: 1,
+    fontFamily: 'sans-serif-medium',
+  },
   secondRowButtons: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -38,13 +60,38 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 15,
   },
+  dropdownMenu: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+    height: 200,
+    width: 200,
+  },
+  dropdownItem: {
+    width: '100%',
+    height: 50,
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#B1BDC8',
+  },
+  dropdownItemText: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+    textAlign: 'center',
+  }
 });
 
-const ChangeTask = ({navigation, id}) => {
+const ChangeTask = ({navigation, ...props}) => {
   const list = useTaskListStore(state => state.taskList);
+  const id = props.id;
   const task = list.find(item => {
     return item.id === id
   })
+
   const [taskTitle, setTaskTitle] = React.useState(task.taskTitle);
   const [taskDate, setTaskDate] = React.useState(task.taskDate);
   const [taskProject, setTaskProject] = React.useState(task.taskProject);
@@ -52,8 +99,30 @@ const ChangeTask = ({navigation, id}) => {
   const [taskTags, setTaskTags] = React.useState(task.taskTags);
 
   const [open, setOpen] = React.useState(false);
+
+  const projectList = useTaskListStore(state => state.projectList);
+  const tagsList = useTaskListStore(state => state.tagsList);
+
+  const projects = ['', ...projectList];
+  const tags = ['', ...tagsList];
+  const priorities = [
+    {
+      key: 0, color: '#6F6F6F',
+    },
+    {
+      key: 1, color: 'blue',
+    },
+    {
+      key: 2, color: 'orange',
+    },
+    {
+      key: 3, color: 'red',
+    },
+  ]
+
   const changeTask = useTaskListStore(state => state.changeTask);
-  const saveNewTask = () => {
+
+  const onChangeTaskClick = () => {
     changeTask(id, task.isCompleted, taskTitle, taskDate, taskProject, taskPriority, taskTags);
     navigation.goBack();
   };
@@ -75,48 +144,105 @@ const ChangeTask = ({navigation, id}) => {
           type="outline"
           radius={'md'}
           buttonStyle={{borderColor: 'coral', borderWidth: 1}}
-          titleStyle={{color: 'coral'}}
+          titleStyle={styles.dateButton}
           onPress={() => setOpen(true)}>
-          <Icon name="calendar-blank-outline" type="material-community" />
+          <Icon name="calendar-blank-outline" size={28} style={{marginRight: 5}} type="material-community" />
           Date
         </Button>
-        <DatePicker
-          modal
-          open={open}
-          date={taskDate}
-          onConfirm={taskDate => {
-            setOpen(false);
-            setTaskDate(taskDate);
-          }}
-          onCancel={() => {
-            setOpen(false);
-          }}
+        <DatePicker modal open={open} date={taskDate}
+          onConfirm={taskDate => {setOpen(false); setTaskDate(taskDate); }}
+          onCancel={() => { setOpen(false); }}
         />
-        <Button
-          type="outline"
-          radius={'md'}
-          buttonStyle={{borderColor: 'indigo', borderWidth: 1}}
-          titleStyle={{color: 'indigo'}}
-          onPress={() => onProjectClick()}>
-          <Icon name="file-table-box-outline" type="material-community" />
-          Project
-        </Button>
+        {/* Project */}
+        <SelectDropdown
+          data={projects}
+          onSelect={(selectedItem, index) => {
+            setTaskProject(selectedItem);
+          }}
+          // button render
+          renderButton={(selectedItem, isOpen) => {
+            return (
+              <View type="outline" radius={'md'} style={styles.projectButton} titleStyle={{color: 'indigo'}} >
+                <Icon name="file-table-box-outline" size={28} style={{marginRight: 5}} type="material-community" />
+                <Text style={styles.projectText}>{selectedItem ? selectedItem : 'Project'}</Text>
+              </View>
+            );
+          }}
+          // item in the list render
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View
+                style={{
+                  ...styles.dropdownItem,
+                  ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                }}>
+                <Text style={styles.dropdownItemText}>{item}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenu}
+        />
       </View>
       <View style={styles.secondRowButtons}>
         <View style={styles.specsButtons}>
-          <Icon
-            name="tag-outline"
-            type="material-community"
-            size={32}
-            color="charcoal"
-            onPress={() => onTagClick()}
+          {/* Tag */}
+          <SelectDropdown
+            data={tags}
+            onSelect={(selectedItem, index) => {
+              setTaskTags(selectedItem);
+            }}
+            // button render
+            renderButton={(selectedItem, isOpen) => {
+              return (
+                <View >
+                  <Icon name="tag-outline" type="material-community" size={40} color="charcoal" />
+                </View>
+              );
+            }}
+            // item in the list render
+            renderItem={(item, index, isSelected) => {
+              return (
+                <View
+                  style={{
+                    ...styles.dropdownItem,
+                    ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                  }}>
+                  <Text style={[styles.dropdownItemText, {color: item.color}]}> {item}</Text>
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={styles.dropdownMenu}
           />
-          <Icon
-            name="flag-variant-outline"
-            type="material-community"
-            size={32}
-            color="charcoal"
-            onPress={() => onPriorityClick()}
+          {/* Priority */}
+          <SelectDropdown
+            data={priorities}
+            onSelect={(selectedItem, index) => {
+              setTaskPriority(selectedItem);
+            }}
+            // button render
+            renderButton={(selectedItem, isOpen) => {
+              return (
+                <View >
+                  <Icon name="flag-variant-outline" type="material-community" size={40} color={selectedItem?.color || '#6F6F6F'} />
+                </View>
+              );
+            }}
+            // item in the list render
+            renderItem={(item, index, isSelected) => {
+              return (
+                <View
+                  style={{
+                    ...styles.dropdownItem,
+                    ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                  }}>
+                  <Text style={[styles.dropdownItemText, {color: item.color}]}>Priority {item.key}</Text>
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={styles.dropdownMenu}
           />
         </View>
         <Button
@@ -130,7 +256,7 @@ const ChangeTask = ({navigation, id}) => {
             size: 24,
             color: 'white',
           }}
-          onPress={saveNewTask}
+          onPress={onChangeTaskClick}
         />
       </View>
     </SafeAreaView>
--- src/screens/MakeTask.js
+++ src/screens/MakeTask.js
@@ -5,6 +5,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import DatePicker from 'react-native-date-picker';
 import {Icon, Button} from '@rneui/themed';
 import {useTaskListStore} from '../utils/store';
+import SelectDropdown from 'react-native-select-dropdown';
 
 const styles = StyleSheet.create({
   container: {
@@ -28,16 +29,60 @@ const styles = StyleSheet.create({
     gap: 15,
     marginBottom: 15,
   },
+  dateButton: {
+    borderColor: 'coral',
+    borderWidth: 1
+  },
+  projectButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 8,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    borderColor: 'indigo',
+    borderWidth: 1,
+  },
+  projectText: {
+    color: 'indigo',
+    fontSize: 16,
+    textAlign: 'center',
+    paddingVertical: 1,
+    fontFamily: 'sans-serif-medium',
+  },
   secondRowButtons: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'baseline',
+    alignItems: 'center',
     width: 360,
   },
   specsButtons: {
     flexDirection: 'row',
     gap: 15,
   },
+  dropdownMenu: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+    height: 200,
+    width: 200,
+  },
+  dropdownItem: {
+    width: '100%',
+    height: 50,
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#B1BDC8',
+  },
+  dropdownItemText: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+    textAlign: 'center',
+  }
 });
 
 const MakeTask = ({navigation}) => {
@@ -48,13 +93,35 @@ const MakeTask = ({navigation}) => {
   const [taskTags, setTaskTags] = React.useState([]);
 
   const [open, setOpen] = React.useState(false);
+
+  const projectList = useTaskListStore(state => state.projectList);
+  const tagsList = useTaskListStore(state => state.tagsList);
+
+  const projects = ['', ...projectList];
+  const tags = ['', ...tagsList];
+  const priorities = [
+    {
+      key: 0, color: '#6F6F6F',
+    },
+    {
+      key: 1, color: 'blue',
+    },
+    {
+      key: 2, color: 'orange',
+    },
+    {
+      key: 3, color: 'red',
+    },
+  ]
+
   const addTask = useTaskListStore(state => state.addTask);
-  const saveNewTask = () => {
+
+  const onAddTaskClick = () => {
     addTask(taskTitle, taskDate, taskProject, taskPriority, taskTags);
     navigation.goBack();
   };
 
-  //TODO: change screen to overlay
+  //TODO: change screen to overlay or modal
   //TODO: make a toast for a task creation cancellation
   //TODO: make a toast warning when trying to add a task without title
 
@@ -67,9 +134,9 @@ const MakeTask = ({navigation}) => {
         value={taskTitle}
       />
       <View style={styles.firstRowButtons}>
-        <Button type="outline" radius={'md'} buttonStyle={{borderColor: 'coral', borderWidth: 1}} titleStyle={{color: 'coral'}}
+        <Button type="outline" radius={'md'} buttonStyle={styles.dateButton} titleStyle={{color: 'coral'}}
           onPress={() => setOpen(true)}>
-          <Icon name="calendar-blank-outline" type="material-community" />
+          <Icon name="calendar-blank-outline" size={28} style={{marginRight: 5}} type="material-community" />
           Date
         </Button>
         <DatePicker modal open={open} date={taskDate}
@@ -78,24 +145,101 @@ const MakeTask = ({navigation}) => {
             setOpen(false);
           }}
         />
-        <Button type="outline" radius={'md'} buttonStyle={{borderColor: 'indigo', borderWidth: 1}} titleStyle={{color: 'indigo'}}
-          onPress={() => onProjectClick()}>
-          <Icon name="file-table-box-outline" type="material-community" />
-          Project
-        </Button>
+        {/* Project */}
+        <SelectDropdown
+          data={projects}
+          onSelect={(selectedItem, index) => {
+            setTaskProject(selectedItem);
+          }}
+          // button render
+          renderButton={(selectedItem, isOpen) => {
+            return (
+              <View type="outline" radius={'md'} style={styles.projectButton} titleStyle={{color: 'indigo'}} >
+                <Icon name="file-table-box-outline" size={28} style={{marginRight: 5}} type="material-community" />
+                <Text style={styles.projectText}>{selectedItem ? selectedItem : 'Project'}</Text>
+              </View>
+            );
+          }}
+          // item in the list render
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View
+                style={{
+                  ...styles.dropdownItem,
+                  ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                }}>
+                <Text style={styles.dropdownItemText}>{item}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenu}
+        />
       </View>
       <View style={styles.secondRowButtons}>
         <View style={styles.specsButtons}>
-          <Icon name="tag-outline" type="material-community" size={32} color="charcoal"
-            onPress={() => onTagClick()}
+          {/* Tag */}
+          <SelectDropdown
+            data={tags}
+            onSelect={(selectedItem, index) => {
+              setTaskTags(selectedItem);
+            }}
+            // button render
+            renderButton={(selectedItem, isOpen) => {
+              return (
+                <View >
+                  <Icon name="tag-outline" type="material-community" size={40} color="charcoal" />
+                </View>
+              );
+            }}
+            // item in the list render
+            renderItem={(item, index, isSelected) => {
+              return (
+                <View
+                  style={{
+                    ...styles.dropdownItem,
+                    ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                  }}>
+                  <Text style={[styles.dropdownItemText, {color: item.color}]}> {item}</Text>
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={styles.dropdownMenu}
           />
-          <Icon name="flag-variant-outline" type="material-community" size={32} color="charcoal"
-            onPress={() => onPriorityClick()}
+          {/* Priority */}
+          <SelectDropdown
+            data={priorities}
+            onSelect={(selectedItem, index) => {
+              setTaskPriority(selectedItem);
+            }}
+            // button render
+            renderButton={(selectedItem, isOpen) => {
+              return (
+                <View >
+                  <Icon name="flag-variant-outline" type="material-community" size={40} color={selectedItem?.color || '#6F6F6F'} />
+                </View>
+              );
+            }}
+            // item in the list render
+            renderItem={(item, index, isSelected) => {
+              return (
+                <View
+                  style={{
+                    ...styles.dropdownItem,
+                    ...(isSelected && {backgroundColor: '#D2D9DF'}),
+                  }}>
+                  <Text style={[styles.dropdownItemText, {color: item.color}]}>Priority {item.key}</Text>
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            dropdownStyle={styles.dropdownMenu}
           />
         </View>
         <Button color="tomato" buttonStyle={{ borderRadius: 20, }}
-          icon={{ name: 'send', type: 'material-community', size: 24, color: 'white', }}
-          onPress={saveNewTask}
+          icon={{ name: 'send', type: 'material-community', size: 28, color: 'white', }}
+          onPress={onAddTaskClick}
         />
       </View>
     </SafeAreaView>
