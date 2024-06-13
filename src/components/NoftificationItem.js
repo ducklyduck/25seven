d2376de3d8eda4d2fa44f569c71dcf0d17c65044
ducklyduck <nivanov475@gmail.com>
2024-06-14 01:03:44 +0500

    noftificationItem cleaning for the exam

--- src/components/NoftificationItem.js
+++ src/components/NoftificationItem.js
@@ -1,7 +1,4 @@
-import React from 'react';
 import notifee from '@notifee/react-native';
-import {View, Button} from 'react-native';
-
 
 async function onDisplayNotification(taskCount) {
   // Request permissions (required for iOS)
@@ -13,30 +10,17 @@ async function onDisplayNotification(taskCount) {
     name: 'Default Channel',
   });
 
-  // Display a notification
-  // try {
     await notifee.displayNotification({
       title: 'You have work to do',
       body: `You have ${taskCount} uncompleted tasks. Don't forget it!`,
       android: {
         channelId,
-        largeIcon: require('../../assets/noftification-icon.png'), // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
+        largeIcon: require('../../assets/noftification-icon.png'),
         pressAction: {
           id: 'default',
         },
       },
     });
-  // } catch {
-  //   console.error(err);
-  // }
 }
-const NoftificationItem = () => {
-  return (
-    <View>
-      <Button title="Display Notification" onPress={() => {}} />
-    </View>
-  );
-};
 
 export default onDisplayNotification;
